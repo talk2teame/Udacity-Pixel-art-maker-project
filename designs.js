4d686cb1d0f73527a12da58c9774dd970b98efea
talk2teame <talk2teame@gmail.com>
2018-06-28 18:41:55 +0100

    add sizePicker and colorPicker handlers

--- designs.js
+++ designs.js
@@ -3,8 +3,49 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+$("#sizePicker").submit(function(e){
+    e.preventDefault();
+    const height = $('#inputHeight').val()
+    const width = $('#inputWidth').val()
+
+    makeGrid(height, width)
+     
+});
+
+
+function makeGrid(height, width) {
 
 // Your code goes here!
+    $('tr').remove();
 
+    for (let i = 0; i < height; i++) {
+        $('#pixelCanvas').append('<tr></tr>');
+    }
+    
+    for (let x = 0; x < width; x++) {
+
+        $('tr').append('<td id=" "></td>');
+        $('tr').append(`<td id="+ "></td>`);
+
+    }
 }
+
+
+function colorCell(){
+    console.log(arguments.values)
+}
+
+
+$('#colorPicker').change(function(){
+  var colorPicked =  $(this).val()
+
+  console.log(colorPicked, 'hahahah')
+})
+
+
+
+
+
+
+
+
