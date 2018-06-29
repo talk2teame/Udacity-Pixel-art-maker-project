6b00e0e17dbd0111e15afe13cd3241933a0fc40b
talk2teame <talk2teame@gmail.com>
2018-06-29 20:18:14 +0100

    wireup clickhandler function

--- designs.js
+++ designs.js
@@ -1,54 +1,15 @@
 // Select color input
+let color;
 // Select size input
+let height, width;
 
 // When size is submitted by the user, call makeGrid()
-
-$("#sizePicker").submit(function(e){
-    e.preventDefault();
-    const height = $('#inputHeight').val()
-    const width = $('#inputWidth').val()
-
-    makeGrid(height, width)
-     
-});
-
-
-
-
-
-function clickHandler(){
-    var that = $(this);
-
-    $(this).on("click", function (event) {
-        let clickedItem = event.target.id
-        let checker = clickedItem.slice(0,4)
-        if(checker === 'cell'){
-            console.log(clickedItem, 'hey hey from the other side');
-
-            // console.log(event.target.id, 'hey man target')
-            // $(this).css('background-color', '#ff0000');
-
-            let color = $('#colorPicker').val();
-            $('td').css('background-color','color');
-        }
-    });
-    // console.log(clickedBtnID, 'hahaha it worked!!!')
-}
-
-
-	//Add color to the cell clicked
-	// $('td').click(function() {
-	// 	let color = $('#colorPicker').val();
-	// 	$('td').css('background-color','color');
-    // });
-    
-
 function makeGrid(height, width) {
 // Your code goes here!
     $('tr').remove();
 
     for (let i = 0; i < height; i++) {
-        $('#pixelCanvas').append('<tr></tr>');
+        $('#pixelCanvas').append('<tr onClick="clickHandler()"></tr>');
     }
     
     for (let x = 0; x < width; x++) {
@@ -62,15 +23,20 @@ function makeGrid(height, width) {
     })
 }
 
+$("#sizePicker").submit(function(e){
+    e.preventDefault();
+     height = $('#inputHeight').val()
+     width = $('#inputWidth').val()
 
+    makeGrid(height, width);   
+});
 
-// $('#colorPicker').change(function(){
-//   var colorPicked =  $(this).val()
-//   console.log(colorPicked, 'hahahah')
-// })
-
-
-
+function clickHandler(){
+    $(document).on('click', "tr td", function (params) {
+        color = $('#colorPicker').val();
+        $(this).css('background-color', color)
+    })
+}
 
 
 
